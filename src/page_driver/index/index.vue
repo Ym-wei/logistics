cfe18e80803defec8913884a71ec599aeea3e9e0
285133472@qq.com <xiaoming>
2022-05-16 23:57:01 +0800

    feat: 送货员订单列表

---
+++ src/page_driver/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="container">
+    <top-header />
+    <store :active="true"/>
+    <store />
+    <store />
+    <store />
+    <store />
+    <store />
+  </view>
+</template>
+
+<script lang="ts" setup>
+import TopHeader from './topHeader.vue'
+import Store from './store.vue'
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: $commonPadding;
+}
+</style>
---
+++ src/page_driver/index/topHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="top-header">
+    <view class="address dot">
+      <text class="text">广深线/0314</text>
+    </view>
+    <text class="name">刘德华</text>
+    <view>订单: <text class="active">6</text>/14</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.top-header {
+  display: flex;
+  align-items: baseline;
+  font-size: map-get($map: $font-sizes, $key: default);
+  color: map-get($map: $font-colors, $key: gray);
+  padding-bottom: 20rpx;
+  border-bottom: 2rpx solid $border-color;
+}
+
+.address {
+  flex: 1;
+  overflow: hidden;
+
+  .text {
+    display: inline-block;
+    color: $theme-color;
+    padding-bottom: 10rpx;
+    border-bottom: 4rpx solid $theme-color;
+  }
+}
+
+.name {
+  margin-right: 30rpx;
+}
+
+.active {
+  color: map-get($map: $font-colors, $key: error);
+}
+</style>
